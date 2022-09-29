f7f858b8e9224ef75ab5851a1ce3cbcb6838da53
SerhatYıldırım <serhat.yildirim@taputeklif.com>
2022-09-30 00:45:08 +0300

    first blood

---
+++ src/main/java/com/sisal/flightplanningapi/FlightPlanningApiApplication.java
@@ -0,0 +1,13 @@
+package com.sisal.flightplanningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlanningApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightPlanningApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sisal/flightplanningapi/FlightPlanningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisal.flightplanningapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlanningApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
